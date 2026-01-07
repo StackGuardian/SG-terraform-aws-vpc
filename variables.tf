550314eadd74ebd2f648351689b1c34aa55b0498
DigbijayineeClara07 <digbijayinee.clara@stackguardian.io>
2026-01-07 16:43:32 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
